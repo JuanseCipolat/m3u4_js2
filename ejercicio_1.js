004b7fb13a330336f7978510a079629aae65ff3e
Juanse Cipolat <jscipolat@gmail.com>
2024-05-31 14:38:55 -0300

    javascript2

---
+++ ejercicio_1.js
@@ -0,0 +1,11 @@
+document.getElementById('cuadrado1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('cuadrado2').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('cuadrado3').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
